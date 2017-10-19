c76d70a5813565e40123c55fca541fd3771a0b28
Luis Gonzalez <luisarmando1234@gmail.com>
2017-10-18 19:25:34 -0500

    Try

--- views/day.php
+++ views/day.php
@@ -5,24 +5,25 @@ use yii\helpers\Url;
 
 $classes = [];
 
-if ( $models ) {
+if ($models) {
     $classes[] = 'activity';
-}
-else {
+} else {
     $classes[] = 'inactivity';
 }
 
-if ( $date->format('Ymd') == date('Ymd') ) {
+if ($date->format('Ymd') == date('Ymd')) {
     $classes[] = 'today';
 }
 
 ?>
 
-<td <?=count($classes)?'class="'.implode(' ',$classes).'"':''?>>
+<td <?= count($classes) ? 'class="' . implode(' ', $classes) . '"' : '' ?>>
+    <?= Html::a('', '#') ?>
     <div class="day-box">
-        <div class="date-box"><?=$date->format('j')?></div>
+        <div class="date-box"><?= $date->format('j') ?></div>
         <div class="model-box">
-            <?=$dayRender?>
+            <?= $dayRender ?>
         </div>
     </div>
+    <?= Html::endTag('a') ?>
 </td>
