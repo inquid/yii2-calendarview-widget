ddec343fd7dcfb226bac766e31e34d9ce86be036
Luis Gonzalez <luisarmando1234@gmail.com>
2017-10-18 21:22:49 -0500

    added fancybox

--- views/calendar.php
+++ views/calendar.php
@@ -1,9 +1,30 @@
-<div id="<?=$id?>" class="calendar">
+<?php
+echo newerton\fancybox\FancyBox::widget([
+    'target' => 'a[rel=fancybox]',
+    'helpers' => false,
+    'mouse' => true,
+    'config' => [
+        'maxWidth' => '90%',
+        'maxHeight' => '90%',
+        'playSpeed' => 7000,
+        'padding' => 0,
+        'fitToView' => false,
+        'width' => '70%',
+        'height' => '70%',
+        'autoSize' => false,
+        'closeClick' => false,
+        'closeBtn' => true,
+        'openOpacity' => true,
+        'arrows' => false
+    ]
+]);
+?>
+<div id="<?= $id ?>" class="calendar">
 
-<?php if ($title):?>
-    <h1><?=$title?></h1>
-<?php endif;?>
+    <?php if ($title): ?>
+        <h1><?= $title ?></h1>
+    <?php endif; ?>
 
-<?=$calendar?>
+    <?= $calendar ?>
 
 </div>
--- views/day.php
+++ views/day.php
@@ -16,14 +16,13 @@ if ($date->format('Ymd') == date('Ymd')) {
 }
 
 ?>
-
 <td <?= count($classes) ? 'class="' . implode(' ', $classes) . '"' : '' ?>>
-    <?= Html::a('', '#') ?>
-    <div class="day-box">
-        <div class="date-box"><?= $date->format('j') ?></div>
-        <div class="model-box">
-            <?= $dayRender ?>
+    <a href="<?= $link . '?id=' . $date->format('w') ?>" class="fancybox.iframe" rel="fancybox">
+        <div class="day-box">
+            <div class="date-box"><?= $date->format('j') ?></div>
+            <div class="model-box">
+                <?= $dayRender ?>
+            </div>
         </div>
-    </div>
-    <?= Html::endTag('a') ?>
+    </a>
 </td>
